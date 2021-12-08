c13a97e80e34f59e0ca39608e38b19a7f74a2e34
Felipe Geremia-Nievinski <fgnievinski@gmail.com>
2021-12-08 14:44:10 -0300

    Update get_reflection_spherical_quasigeo.m

--- get_reflection_spherical_quasigeo.m
+++ get_reflection_spherical_quasigeo.m
@@ -7,7 +7,7 @@ function [delay, graz_ang, arc_len, slant_dist, X_spec, Y_spec, X_trans, Y_trans
 % - X_spec, Y_spec: reflection point coordinates in quasi-geocentric frame (matrices, in meters)
 % - X_trans, Y_trans: transmitter point coordinates in quasi-geocentric frame (matrices, in meters)
 
-    [delay, graz_ang, arc_len, slant_dist, X_spec, Y_spec, X_trans, Y_trans, elev_spec] = function get_reflection_spherical (varargin{:});
+    [delay, graz_ang, arc_len, slant_dist, X_spec, Y_spec, X_trans, Y_trans, elev_spec] = get_reflection_spherical (varargin{:});
     [X_spec, Y_spec] = get_quasigeo_coord (x_spec, y_spec);
     [X_trans, Y_trans] = get_quasigeo_coord (x_trans, y_trans);
 end
