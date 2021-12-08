a7d8c999b22f64a567aaf6cf5dbe70fcebe948be
Vitor Hugo de Almeida Junior <89110447+vitorhjr@users.noreply.github.com>
2021-12-08 15:25:45 -0300

    Update demo3.m

--- demo3.m
+++ demo3.m
@@ -1,7 +1,7 @@
 Ha_max = 1000;
 Ha_step = 1;
 Has = 0:Ha_step:Ha_max
-ehorz = get_horizon_elevation_angle(Has);
+ehorz = get_spherical_reflection_horizon_elev (Has);
 
 figure
 plot(Has, ehorz, '--r','LineWidth',3)
